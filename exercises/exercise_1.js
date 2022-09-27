bc9e75e891202421ba93c73386ba57bf2c503128
uncogito <ulli.rozen@gmail.com>
2022-09-27 20:46:31 +0200

    Revert "solvevd_first_bug_passed_test"
    This reverts commit 609568d589ab6b2500a2eec37106c24c448fa7c0.

--- exercises/exercise_1.js
+++ exercises/exercise_1.js
@@ -3,11 +3,7 @@ const characters = require("../data/characters.json");
 /* TODO: return one character with an "unknown" birth year  */
 
 const findCharacterUnknownBirthDate = () => {
-  for (let i = 0; i < characters.length; i++) {
-    if (characters[i].birth_year = "unknown") {
-      return characters[i];
-    }
-  }
+  // CODE HERE
 };
 
 module.exports = findCharacterUnknownBirthDate;
