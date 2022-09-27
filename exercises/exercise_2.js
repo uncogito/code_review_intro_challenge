1dfa476964286ad98ee6bae0ee4f3fa9a8ad7ca2
uncogito <ulli.rozen@gmail.com>
2022-09-27 20:56:28 +0200

    second_exercise_solved

--- exercises/exercise_2.js
+++ exercises/exercise_2.js
@@ -2,7 +2,7 @@ const characters = require("../data/characters.json");
 
 // TODO: return all characters with known birth year
 const charactersWithKnownBirthDate = () => {
-  // CODE HERE
+  return characters.filter((e) => e.birth_year !== "unknown")
 };
 
 module.exports = charactersWithKnownBirthDate;
