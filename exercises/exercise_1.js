609568d589ab6b2500a2eec37106c24c448fa7c0
uncogito <ulli.rozen@gmail.com>
2022-09-27 20:39:50 +0200

    solvevd_first_bug_passed_test

--- exercises/exercise_1.js
+++ exercises/exercise_1.js
@@ -3,7 +3,11 @@ const characters = require("../data/characters.json");
 /* TODO: return one character with an "unknown" birth year  */
 
 const findCharacterUnknownBirthDate = () => {
-  // CODE HERE
+  for (let i = 0; i < characters.length; i++) {
+    if (characters[i].birth_year = "unknown") {
+      return characters[i];
+    }
+  }
 };
 
 module.exports = findCharacterUnknownBirthDate;
